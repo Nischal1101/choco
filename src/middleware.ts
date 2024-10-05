ad6ea2e314ebcc8ea2026abaf6896fa1bd0ae407
nischal1101 <kaflenk123@gmail.com>
2024-10-05 07:56:00 +0545

    fix bug in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,19 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth({
   callbacks: {
     authorized: ({ token, req }) => {
+      console.log("token", token);
+
       if (req.nextUrl.pathname.startsWith("/admin")) {
-        token?.role === "admin";
+        return token?.role === "admin";
+      } else if (req.nextUrl.pathname.startsWith("/account")) {
+        return token?.role === "customer";
       } else {
-        return true;
+        return false;
       }
     },
   },
 });
+
 export const config = {
-  matcher: ["/admin(.*)?"],
+  matcher: ["/admin(/.*)?", "/account(/.*)?"],
 };
